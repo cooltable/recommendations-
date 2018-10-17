0bb3eea021a7b48f882d2a0a6a7a107a781f969f
Lauren <llwor94@gmail.com>
2018-10-16 19:36:08 -0700

    udpated login styling

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -41,3 +41,20 @@ export default {
     />
   </div>
 </template>
+
+<style lang="scss" scoped>
+@import '../variables';
+.login {
+  text-align: center;
+  max-width: 550px;
+  margin: 0 auto;
+
+  h1 {
+    margin: 30px 0;
+    text-transform: uppercase;
+    letter-spacing: 5px;
+    color: $color-primary;
+    font-weight: normal;
+  }
+}
+</style>
