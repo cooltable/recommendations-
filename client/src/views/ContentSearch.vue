646bce2e875c84f62ae9aed858a90ff1fdf5df98
Lauren <llwor94@gmail.com>
2018-10-16 23:54:47 -0700

    moving into another component search

---
+++ client/src/views/ContentSearch.vue
@@ -0,0 +1,80 @@
+<script>
+import axios from 'axios';
+import { mapState } from 'vuex';
+
+export default {
+  name: 'contentSearch',
+  props: ['type', 'title', 'toFriends'],
+  computed: mapState({
+    friends: state => state.friends,
+    types: state => state.types,
+  }),
+  methods: {
+    handleSearch: function() {
+      if (this.type === 'movie' || this.type === 'series') {
+        let search = this.title.replace(/\s/g, '+');
+        axios
+          .get(
+            `http://www.omdbapi.com/?s=${search}&type=${
+              this.type
+            }&apikey=30939ebf`,
+          )
+          .then(
+            response => console.log(response.data),
+            // ((this.poster = response.data.Poster)),
+            // (this.isSearch = false)
+          );
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <form v-on:submit.prevent="handleSearch">
+  <div class="row">
+    <p>Recommend a </p>
+    <v-select class="type" :value="type" :options="types" label="type"></v-select>
+  </div>
+  <div class="row">
+    <p>to</p>
+    <v-select multiple :value="toFriends" :options="friends" label="username"></v-select>
+  </div>
+  <div class="row">
+    <input v-if="type" class="input" :value="title" placeholder="Title...">
+    <input class="search" v-if="title" @click.prevent="handleSearch" value="Search" type="submit">
+  </div>
+  </form>
+</template>
+
+<style scoped lang='scss'>
+@import '../variables';
+
+.type {
+  width: 200px;
+}
+
+.row {
+  display: flex;
+  align-items: flex-end;
+  // justify-content: center;
+  margin: 5px 0;
+  p {
+    padding-right: 5px;
+  }
+}
+
+.search {
+  display: inline-block;
+  padding: 0.35em 1.2em;
+  border: 1px solid rgba(60, 60, 60, 0.26);
+  margin: 0 0.3em 0.3em 0;
+  border-radius: 0.12em;
+  box-sizing: border-box;
+  text-decoration: none;
+  font-weight: 300;
+  color: rgba(60, 60, 60, 0.8);
+  margin-bottom: 5px;
+  text-align: center;
+}
+</style>
--- client/src/views/NewContent.vue
+++ client/src/views/NewContent.vue
@@ -1,7 +1,6 @@
 <script>
 import Modal from '../components/Modal';
-import axios from 'axios';
-import { mapState } from 'vuex';
+import ContentSearch from './ContentSearch';
 import { StarRating } from 'vue-rate-it';
 
 export default {
@@ -10,18 +9,16 @@ export default {
   components: {
     Modal,
     StarRating,
+    ContentSearch,
   },
-  computed: mapState({
-    friends: state => state.friends,
-    types: state => state.types,
-  }),
+
   data: function() {
     return {
       isSearch: true,
       showModal: false,
       title: '',
       type: '',
-      tofriends: [],
+      toFriends: [],
       poster: '',
       rating: 0,
 
@@ -30,8 +27,8 @@ export default {
   },
   methods: {
     handleSubmit: function() {
-      console.log(this.tofriends);
-      let to_idARR = this.tofriends.map(friend => {
+      console.log(this.toFriends);
+      let to_idARR = this.toFriends.map(friend => {
         console.log(friend.id);
         return friend.id;
       });
@@ -43,18 +40,6 @@ export default {
         to_idARR: to_idARR,
         image: this.poster,
       });
-    },
-    handleSearch: function() {
-      if (this.type.type === 'movie') {
-        let movie = this.title.replace(/\s/g, '+');
-        axios
-          .get(`http://www.omdbapi.com/?t=${movie}&apikey=30939ebf`)
-          .then(
-            response => (
-              (this.poster = response.data.Poster), (this.isSearch = false)
-            ),
-          );
-      }
     },
     handleClose: function() {
       Object.assign(this.$data, this.$options.data.call(this));
@@ -73,29 +58,19 @@ export default {
       <h1 slot="header">New Req</h1>
       <div slot="body">
 
-        <form v-on:submit.prevent="handleSubmit">
-          <div v-if="isSearch">
-            <div class="row">
-              <p>Recommend a </p>
-              <v-select class="type" v-model="type" :options="types" label="type"></v-select>
-            </div>
-            <div class="row">
-              <p>to</p>
-              <v-select multiple v-model="tofriends" :options="friends" label="username"></v-select>
-            </div>
-            <div class="row">
-              <input v-if="type" class="input" v-model="title" placeholder="Title...">
-              <button class="search" v-if="title" @click.prevent="handleSearch" >Go</button>
-            </div>
-          </div>
+        
+          <content-search v-if="isSearch" :type.sync="this.type.type" :title.sync="title" :toFriends.sync="toFriends"/>
+            
           <div v-else>
+          <form v-on:submit.prevent="handleSubmit">
             <p>Recommend {{title}} to {{tofriends.map(friend => friend.username)}}</p>
             <img :src="poster">
             <star-rating v-model="rating"></star-rating>
             <textarea v-model="description" placeholder="Description"></textarea>
             <input :disabled="!rating" value="Recommend" type="submit">
+          </form>
           </div>
-        </form>
+        
 
       </div>
     </modal>
@@ -135,34 +110,6 @@ form {
 
 .Main__recButton p {
   margin-right: 8px;
-}
-
-.type {
-  width: 200px;
-}
-
-.row {
-  display: flex;
-  align-items: flex-end;
-  // justify-content: center;
-  margin: 5px 0;
-  p {
-    padding-right: 5px;
-  }
-}
-
-.search {
-  display: inline-block;
-  padding: 0.35em 1.2em;
-  border: 1px solid rgba(60, 60, 60, 0.26);
-  margin: 0 0.3em 0.3em 0;
-  border-radius: 0.12em;
-  box-sizing: border-box;
-  text-decoration: none;
-  font-weight: 300;
-  color: rgba(60, 60, 60, 0.8);
-  margin-bottom: 5px;
-  text-align: center;
 }
 
 .input {
